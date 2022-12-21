cde186658c6690a48a8da0d09bf110bf838113d0
Daniel <danilagalakov3@gmail.com>
2022-12-22 04:03:32 +0600

    second

--- src/header/Theme.jsx
+++ src/header/Theme.jsx
@@ -1,19 +1,31 @@
 import Style from '../header/Header.module.css';
 const Theme = () => {
     const htmlDOM = document.querySelector('html');
+    console.log(window.matchMedia('(prefers-color-scheme: dark)').matches)
 
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    console.log(window.localStorage.getItem('theme'))
+    window.localStorage.setItem('theme', '');
+
+
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches && window.localStorage.getItem('theme')=== '') {
+        window.localStorage.setItem('theme', 'dark');
         htmlDOM.classList.add('dark');
-    } else {
+    } 
+    else if(window.matchMedia('(prefers-color-scheme: light)').matches && window.localStorage.get('theme')=== '' ) {
         htmlDOM.classList.add('light');
+        window.localStorage.setItem('theme', 'light');
+        // htmlDOM.classList.add('dark');
     }
+
 
     if(window.localStorage.getItem('theme') === 'dark' ) {
         htmlDOM.classList.add('dark');
         htmlDOM.classList.remove('light');
+        window.localStorage.setItem('theme', 'dark');
     } else {
         htmlDOM.classList.add('light');
         htmlDOM.classList.remove('dark');
+        window.localStorage.setItem('theme', 'light');
     }
 
     return (
