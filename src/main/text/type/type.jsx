a3543c58e7d6fb9ecc28df7c4438c3a7ecc8c762
Daniel <danilagalakov3@gmail.com>
2023-01-04 22:21:26 +0600

    fix

--- src/main/text/type/type.jsx
+++ src/main/text/type/type.jsx
@@ -327,18 +327,19 @@ const inputCheck = (event)=> {
             if(inputText.current[i] === event.nativeEvent.data) {
                 simbolsCount++;
                 console.log('simbols'+ simbolsCount)
+                
+                console.log('ввод i ид ' +  inputText.current[i])
+                console.log( 'ТО ЧТо нажали' +event.nativeEvent.data)
             }
-            console.log('ввод i ид ' +  inputText.current[i])
-            // console.log('ввод 0 ид' + inputText.current[0])
-            console.log( 'ТО ЧТо нажали' +event.nativeEvent.data)
     }
     else {
         i--
         setTimeout(backSpace, 0);
         errorCount++;
         console.log('errors'+ errorCount)
-        console.log(inputText.current[i])
-        console.log(event.nativeEvent.data)
+
+        console.log('ввод i ид ' +  inputText.current[i])
+        console.log( 'ТО ЧТо нажали' +event.nativeEvent.data)
     }
 }
 
