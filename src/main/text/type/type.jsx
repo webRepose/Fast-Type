39836e2d312bd3c097769168109000b80a5824b3
Daniel <danilagalakov3@gmail.com>
2023-01-09 17:04:53 +0600

    fix

--- src/main/text/type/type.jsx
+++ src/main/text/type/type.jsx
@@ -170,17 +170,12 @@ const inputCheck = (event)=> {
     }
 
     if(inputText[i-1] !== undefined || null) {
-        // alert(inputText[i-1])
         if(/Android/i.test(navigator.userAgent)) {
-            // inputText[i-1] = ''
-            // alert(event.nativeEvent.data[i-2])
-            // alert(event.nativeEvent.data[i-1] = '')
-            alert(event.nativeEvent.data[i-1].value)
-            // alert(inputText[i+1])
-            // alert(inputText[i])
-            // alert(event.nativeEvent.data)
+            // alert(event.nativeEvent.data[i-1].value)
+            alert(event.nativeEvent.data[i-1])
         }
     }
+    console.log(event.nativeEvent.data[i-1])
     if(inputText[i] === event.nativeEvent.data) {
         setI(prev => prev+1);
         setSimbols(prev => prev+1);
