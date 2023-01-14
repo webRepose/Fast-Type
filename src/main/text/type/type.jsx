5046a772470bffeaeed78955b38ca31bc3fe036a
Daniel <danilagalakov3@gmail.com>
2023-01-14 22:13:39 +0600

    new

--- src/main/text/type/type.jsx
+++ src/main/text/type/type.jsx
@@ -180,7 +180,7 @@ const inputCheck = (event)=> {
 
         //     // event.target.value.substr(-2,event.target.value.lengяth - 1)
         // }
-        alert(event.target.value.substr(0))
+        alert(event.target.value.substr(-1))
     }
 
     if(inputText[i] === event.nativeEvent.data) {
