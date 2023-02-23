2902e7675870011c96bf76c152515d4cd67bd262
Daniel <danilagalakov3@gmail.com>
2023-02-23 21:04:56 +0600

    test

--- src/main/text/Typing/Typing.jsx
+++ src/main/text/Typing/Typing.jsx
@@ -120,12 +120,12 @@ const inputCheck = (event) => {
             }
     }
     } else if(/Android|HarmonyOS/i.test(navigator.userAgent)) {
-        // let b = inputArea.current.value = '';
         if(d + inputText[i] === event.nativeEvent.data) {
-            if(inputArea.current.value.length >= 1) d = inputArea.current.value;
             console.log(inputArea.current.value.length)
+            alert('value', d)
             console.log(d)
-            alert(d)
+            alert('inp index', inputText[i]);
+            alert('nativeDate ',event.nativeEvent.data)
             setI(prev => prev + 1);
             setSimbols(prev => prev + 1);
             if(event.nativeEvent.data === ' ' || event.nativeEvent.data === '-' ) {
@@ -134,17 +134,19 @@ const inputCheck = (event) => {
             setTimeout(() => {
                 console.log(inputArea.current.value + inputText[i+1]);
             }, 0);
+            
+            if(inputArea.current.value.length >= 1) d = inputArea.current.value;
 
-            alert(inputArea.current.value)
-            alert(inputText[i+1])
+            // alert(inputArea.current.value)
+            // alert(inputText[i+1])
         }   else {
             backSpace();
             inputBlock.current.style.border = '1px solid red';
             setTimeout(()=>{inputBlock.current.style.border = '1px solid #707070'}, 500);
             setErrorCount(prev => prev+1);
 
-            alert(inputArea.current.value)
-            alert(inputText[i+1])
+            // alert(inputArea.current.value)
+            // alert(inputText[i+1])
     }
     };
 
