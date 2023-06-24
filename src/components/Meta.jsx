55e4676beeac9c6f75493699fd5eda7ecbf9baf9
Daniel <danilagalakov3@gmail.com>
2023-06-24 22:54:38 +0600

    rew

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -6,7 +6,7 @@ const Meta = ({description, title, keywords, canon}) => {
         <Helmet>
             <title>{title}</title>
             <meta name="description" content={description}></meta>
-            <link rel="canonical" href={`https://fast-type-red.vercel.app/${canon}`} />
+            <link rel="canonical" href={`https://fasttyping.vercel.app/${canon}`} />
             <meta name="keywords" content={keywords}></meta>
             <meta name="apple-mobile-web-app-title" content={title}></meta>
             <meta property="og:title" content={title}></meta>
